b035c49c323d86e2a2ccee69553a0e8d9f8d7ab9
Wolela Woldemicheal Woldeselassie <wolelawm050@gmail.com>
2024-11-19 10:32:51 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone/",
+  server: {
+    port: 3000,
+  },
+  base: "/React-Apple-Replica-Project/",
 });
