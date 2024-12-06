e50e8e93c966c9bc09a79fe1372227b20204474c
Wolela Woldemicheal Woldeselassie <wolelawm050@gmail.com>
2024-12-06 14:55:45 +0300

    app.jsx header import path updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./Components/Header/header";
+import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Banner from "./Components/Banner/Banner";
 import RowList from "./Components/Rows/RowList/RowList";
